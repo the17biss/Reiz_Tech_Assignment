7618a61b5423555b988851861d69fb19a4c6ecaa
Bishal <b.humagai17@gmail.com>
2023-05-19 21:46:20 +0545

    Assignment Completed

---
+++ Q_no_1_ConsoleApp/Q_no_1_ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static double CalculateAngle(int hours, int minutes)
+    {
+        // Converting hours to 12-hour format
+        hours %= 12;
+        /* The angle covered by minute hand is calculated as:
+        * total angle covered by clock = 360
+        * total minutes in a complete hour = 60
+        * so, each minute mark = 360/60 = 6
+        */
+        double minuteAngle = minutes * 6;
+
+        /* The angle covered by hour is calculated as:
+         * Since we have tweleve hours in a clock which is a perfect circle having complete angle of 360 degrees.
+         * So each hour mark = 360/12 = 30 degrees
+         * But hour position is influenced by the minutes position too so:
+         * actual hour angle is equal to sum of hour *30 + minutes * 30/60..
+         */
+
+        double hourAngle = (hours * 30) + (minutes * 0.5);
+
+
+        // Calculating the absolute difference between the two angles which gives the angle between the hour hand and minute hand
+        double angle = Math.Abs(hourAngle - minuteAngle);
+
+        // Calculating the lesser angle (360 - angle) when the angle is greater than 180 degrees
+        if (angle > 180)
+        {
+            angle = 360 - angle;
+        }
+
+        return angle;
+    }
+
+    static void Main()
+    {
+        // Asking for user input about hour mark and minute mark:
+        Console.Write("Enter the hours (0-23): ");
+        int hours = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the minutes (0-59): ");
+        int minutes = Convert.ToInt32(Console.ReadLine());
+
+        // Calculating the lesser angle by calling the method defined above:
+        double angle = CalculateAngle(hours, minutes);
+
+        // Output the result
+        Console.WriteLine($"The lesser angle between the hour and minute hands is {angle} degrees.");
+    }
+}
---
+++ Q_no_2_Hierarchial_Structure/Q_no_2_Hierarchial_Structure/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Branch
+{
+    private List<Branch> branches;
+
+    public Branch()
+    {
+        branches = new List<Branch>();
+    }
+
+    public void AddBranch(Branch branch)
+    {
+        branches.Add(branch);
+    }
+
+    public List<Branch> GetBranches()
+    {
+        return branches;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create the hierarchical structure
+        Branch root = new Branch();
+        Branch branch1 = new Branch();
+        Branch branch2 = new Branch();
+        Branch branch3 = new Branch();
+        Branch branch4 = new Branch();
+        Branch branch5 = new Branch();
+        Branch branch6 = new Branch();
+        Branch branch7 = new Branch();
+        Branch branch8 = new Branch();
+        Branch branch9 = new Branch();
+        Branch branch10 = new Branch();
+        Branch branch11 = new Branch();
+
+        root.AddBranch(branch1);
+        root.AddBranch(branch2);
+        branch1.AddBranch(branch3);
+        branch2.AddBranch(branch4);
+        branch2.AddBranch(branch5);
+        branch2.AddBranch(branch6);
+        branch4.AddBranch(branch7);
+        branch5.AddBranch(branch8);
+        branch5.AddBranch(branch9);
+        branch8.AddBranch(branch10);
+        
+
+
+        // Calculating the depth of the structure
+        int depth = CalculateDepth(root);
+
+        Console.WriteLine("The depth of the structure is: " + depth);
+    }
+
+    static int CalculateDepth(Branch node)
+    {
+        if (node.GetBranches().Count == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            int maxChildDepth = 0;
+            foreach (Branch child in node.GetBranches())
+            {
+                int childDepth = CalculateDepth(child);
+                maxChildDepth = Math.Max(maxChildDepth, childDepth);
+            }
+            return 1 + maxChildDepth;
+        }
+    }
+}
